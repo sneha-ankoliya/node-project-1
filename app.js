dcc53c7fa9f6f8b98e7f232a071626e0eca3f597
sneha ankoliya <snehaankoliya22@gmail.com>
2026-01-07 18:55:25 +0530

    [SA][main]-app.js file comment codde added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 3001; // or any other port number you prefer
 
+//this is changes
+
 app.use(express.json());
 
 const users = [ 
